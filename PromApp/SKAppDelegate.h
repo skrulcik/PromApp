7e3dc1a7b361f00c182704f7c4da54046a572039
Scott Krulcik <Scott@Scotts-MacBook-Pro.local>
2014-06-30 18:17:51 -0400

    Initial Commit

---
+++ PromApp/SKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKAppDelegate.h
+//  PromApp
+//
+//  Created by Scott Krulcik on 6/30/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
