72a263816a975b4939038c88e9b9759ce50dc8ac
Scott Krulcik <Scott@SKrulcik-MBP.local>
2014-08-03 12:59:01 -0400

    Added StringEntryCell -Holds a text field so a table can be built with editing capabilities -Meant to work with key:value object editors, so a “key” can be associated with the cell 	-More specifically this means that if a large table of these cells are being used to edit values related to a key:value coding compliant object, the cell can be identified by the key that its contents correspond to.

---
+++ PromApp/SKStringEntryCell.m
@@ -0,0 +1,36 @@
+//
+//  SKTableViewCell.m
+//  PromApp
+//
+//  Created by Scott Krulcik on 8/2/14.
+//  Copyright (c) 2014 Scott Krulcik. All rights reserved.
+//
+
+#import "SKStringEntryCell.h"
+
+@implementation SKStringEntryCell
+@synthesize key;
+@synthesize field;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
