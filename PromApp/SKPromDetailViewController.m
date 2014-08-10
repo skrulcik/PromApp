fd1b0668f1f11636632f50bfc64087a82f2d121d
Scott Krulcik <skrulcik@gmail.com>
2014-08-10 01:25:52 -0400

    Removed unnecessary warnings

--- PromApp/SKPromDetailViewController.m
+++ PromApp/SKPromDetailViewController.m
@@ -52,14 +52,12 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
     return 1+[self.prom.dresses count];
 }
