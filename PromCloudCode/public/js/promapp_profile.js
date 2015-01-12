40892ea81fd0e6d3b8c29913506923ed497135b1
Scott Krulcik <skrulcik@gmail.com>
2015-01-12 18:01:48 -0500

    Load dress information into profile, load images from facebook profiles.

--- PromCloudCode/public/js/promapp_profile.js
+++ PromCloudCode/public/js/promapp_profile.js
@@ -1,35 +1,103 @@
 $(function() {
+    //Verify page with Parse
+    Parse.$ = jQuery;
+    Parse.initialize("PJq63qVW5giu8JBkupPxHADBgSpMEEX87QlZjDlg", "QDNCcyQqQRlCjMSIrXanH37MioTb2IJoPIuD5d1C");
 
-//Temporary before users are available
-var dresses = [{designer:"Faviana", styleNumber:"fe456"},{designer:"Jovani", styleNumber:"JV-967"}];
-
-function loadDresses(){
-	$('#dresslist').empty(); //Clears out existing dress list views
-	$.each(dresses, function(index, dress) {
-		var dressInfo = document.createElement('div');
-		dressInfo.setAttribute("class", "col-sm-8 col-md-3");
-		var dressInfo_thumb = document.createElement('div');
-		dressInfo_thumb.setAttribute("class", "thumbnail dress");
-		var dressInfo_thumb_img = document.createElement('img');
-		dressInfo_thumb_img.setAttribute("data-src", "holder.js/200x300");
-		//TODO: fill in dress with real data
-		var dressInfo_thumb_caption = document.createElement('div');
-		dressInfo_thumb_caption.setAttribute('class', 'caption');
-		var caption_designer = document.createElement('h4');
-		var caption_styleNumber = document.createElement('h5');
-		caption_designer.innerHTML = dress.designer;
-		caption_styleNumber.innerHTML = dress.styleNumber;
-		//Add children to parent elements
-		dressInfo_thumb_caption.appendChild(caption_designer);
-		dressInfo_thumb_caption.appendChild(caption_styleNumber);
-		dressInfo_thumb.appendChild(dressInfo_thumb_img);
-		dressInfo_thumb.appendChild(dressInfo_thumb_caption);
-		dressInfo.appendChild(dressInfo_thumb);
-		$('#dresslist').append(dressInfo);
-	});
-}
-
-loadDresses();
+    //Temporary before users are available
+    var dresses = [{designer:"Faviana", styleNumber:"fe456"},{designer:"Jovani", styleNumber:"JV-967"}];
+    function optional_s(num){
+        return (num == 1) ? "":"s";
+    }
+    function optional_es(num){
+        return (num == 1) ? "":"es";
+    }
+    function loadProfile(){
+        //Get common name of user from Parse (stored in profile attribute to maintain consistency with FB)
+        var current = Parse.User.current();
+        var profile = current.get("profile");
+        if(profile){
+            var fullName = profile["first_name"] + " " + profile["last_name"];
+            $("#username").html(fullName);
+        }
+        loadProfImage(current);
+        var numDresses = 0;
+        if(current.get("dresses")){
+            var dresses = current.get("dresses");
+            numDresses = dresses.length;
+            loadDresses(dresses);
+        }
+        if(numDresses == 0){
+            $("#dresses").hide();
+        }
+        var numProms = 0;
+        if(current.get("proms")){
+            proms = current.get("proms");
+            numProms = proms.length;
+        }
+        if(numProms == 0){
+            $("#proms").hide();
+        }
+        //Display numerical data on page
+        $("#dressNumber").html(numDresses + " Dress" + optional_es(numDresses));
+        $("#promNumber").html(numProms  + " Prom" + optional_s(numProms));
+    }
+    function loadDresses(dresses){
+        $('#dresslist').empty(); //Clears out existing dress list views
+        $.each(dresses, function(index, dress) {
+            //Create a container for dress information, then subviews
+            var dressInfo = document.createElement('div');
+            dressInfo.setAttribute("class", "col-sm-8 col-md-3");
+            var dressInfo_thumb = document.createElement('div');
+            dressInfo_thumb.setAttribute("class", "thumbnail dress");
+            var dressInfo_thumb_img = document.createElement('img');
+            dressInfo_thumb_img.setAttribute('class', 'img-responsive');
+            var dressInfo_thumb_caption = document.createElement('div');
+            dressInfo_thumb_caption.setAttribute('class', 'caption');
+            var caption_designer = document.createElement('h4');
+            var caption_styleNumber = document.createElement('h5');
+            //Retrieve data from model to fill in information
+            dress.fetch({
+              success: function(myObject) {
+                caption_designer.innerHTML = dress.get("designer");
+                caption_styleNumber.innerHTML = dress.get("styleNumber");
+                //Retrieve stored picture from model
+                var dressPic = dress.get("image");
+                if (dressPic) {
+                    dressInfo_thumb_img.src = dressPic.url();
+                } else {
+                    dressInfo_thumb_img.setAttribute("data-src", "holder.js/200x300");
+                }
+              },
+              error: function(myObject, error) {
+                // The object was not refreshed successfully.
+                // error is a Parse.Error with an error code and message.
+                alert("Server Error: ", error.code);
+              }
+            });
+            //Add children to parent elements
+            dressInfo_thumb_caption.appendChild(caption_designer);
+            dressInfo_thumb_caption.appendChild(caption_styleNumber);
+            dressInfo_thumb.appendChild(dressInfo_thumb_img);
+            dressInfo_thumb.appendChild(dressInfo_thumb_caption);
+            dressInfo.appendChild(dressInfo_thumb);
+            //Append the view for the dress into the list section
+            $('#dresslist').append(dressInfo);
+        });
+    }
+    function loadProfImage(user){
+        if(Parse.FacebookUtils.isLinked(user)){
+            var URL = "https://graph.facebook.com/"+ user.get("profile")["id"]+"/picture?type=large";
+            $('#profile img').attr("src", URL);
+        } else {
+            // For non-facebook users, get image if it exists
+            var image = user.get("image");
+            if(image && image.url() != ""){
+                $('#profile img').attr("src", image.url());
+            }
+        }
+        
+    }
+    loadProfile();
 });
 
 
--- PromCloudCode/public/js/promapp_signup.js
+++ PromCloudCode/public/js/promapp_signup.js
@@ -21,15 +21,14 @@ $(function(){
 	    var js, fjs = d.getElementsByTagName(s)[0];
 	    if (d.getElementById(id)) {return;}
 	    js = d.createElement(s); js.id = id;
-	    js.src = "http://connect.facebook.net/en_US/sdk.js";
+	    js.src = "https://connect.facebook.net/en_US/sdk.js";
 	    fjs.parentNode.insertBefore(js, fjs);
 	  }(document, 'script', 'facebook-jssdk'));
 
 	function facebookLogin(){
-		Parse.FacebookUtils.init();
+		//Parse.FacebookUtils.init();
 		Parse.FacebookUtils.logIn(null, {
 		  success: function(user) {
-		  	alert("Successful PromApp Login!");
 		    if (!user.existed()) {
 		      window.location = 'profile.html';
 		    } else {
