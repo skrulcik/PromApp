6cace746aeccf7f4ea317880645561ec95492fed
Scott Krulcik <skrulcik@gmail.com>
2015-01-16 08:36:34 -0500

    Added support question to explain facebook login. Changed FAQ to SUPPORT on main menu. Attempted to arrange index page better on medium size screens. Removed unused code from profile (noticed it had not been removed in last commit).

--- PromCloudCode/public/js/promapp_profile.js
+++ PromCloudCode/public/js/promapp_profile.js
@@ -1,13 +1,3 @@
-// goto function from:
-// http://stackoverflow.com/questions/4801655/how-to-go-to-a-specific-element-on-page
-(function($) {
-    $.fn.goTo = function() {
-        $('html, body').animate({
-            scrollTop: $(this).offset().top + 'px'
-        }, 'fast');
-        return this; // for chaining...
-    }
-})(jQuery);
 
 $(function() {
     //Verify page with Parse
