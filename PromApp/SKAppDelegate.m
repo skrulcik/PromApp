0faf5a1d13b8eb164498274aeb9b8156452f8316
Scott Krulcik <skrulcik@gmail.com>
2015-01-24 16:05:26 -0500

    Add app open analytics

--- PromApp/SKAppDelegate.m
+++ PromApp/SKAppDelegate.m
@@ -26,7 +26,7 @@
                   clientKey:@"cxqrUMU3wX4UA67IvLgqv0aT78dhVY1DT3w8LWIt"];
     // Facebook initialization
     [PFFacebookUtils initializeFacebook];
-    //[Parse enableLocalDatastore];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     
     //Navigation Bar
     // Set the global tint on the navigation bar
