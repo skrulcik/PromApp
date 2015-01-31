6fc2a558e83d49a6c481646238df2415721982b5
Scott Krulcik <skrulcik@gmail.com>
2015-01-30 22:54:37 -0500

    Added store and prom creation to web portal - Added Prom Search feature and follow prom button

--- PromCloudCode/public/js/promapp_profile.js
+++ PromCloudCode/public/js/promapp_profile.js
@@ -9,6 +9,7 @@ $(function() {
     var Prom = Parse.Object.extend("Prom");
     var Dress = Parse.Object.extend("Dress");
     var Store = Parse.Object.extend("Store");
+    var QUERY_LIMIT = 25;
     
     /* Underscore Templates ******************/
     // Access template variables using data.<varname>
@@ -16,6 +17,8 @@ $(function() {
     _.templateSettings.variable = "data";
     // Pre-compile templates for fast reuse
     var EditTemplate = _.template($("script.editor_template").html());
+    var PromSearchItemTemplate = _.template($('script.promsearchitem').html());
+    var PromDetailRowTemplate = _.template($('script.promdetailrow').html());
 
     /* Template Data *************************/
     // Instructions for Underscore to use to construct
@@ -257,6 +260,7 @@ $(function() {
                     newProm.set('address', address);
                     // Geocode address with google api
                     var geocoder = new google.maps.Geocoder();
+                    console.log("Geocoding: "+address);
                     geocoder.geocode( { 'address': address}, function(results, status) {
                       if (status == google.maps.GeocoderStatus.OK) {
                         var loc_lat = results[0].geometry.location.lat();
@@ -264,7 +268,9 @@ $(function() {
                         var location = new Parse.GeoPoint({latitude: loc_lat, longitude: loc_long});
                         newProm.set('preciseLocation', location);
                         newProm.save(); // Save again, asynch, so may not be set for initial save
-                      } 
+                      } else {
+                          console.log("Error with geocoding string." + status);
+                      }
                     });
                 }
                 return newProm.save();
@@ -282,11 +288,27 @@ $(function() {
         } else {
             // Set default fields
             newProm.set('schoolName', schoolName);
-            newProm.set('address', address);
             newProm.set('locationDescription', locationDescription);
             // Set optional fields
             if(theme){
                 newProm.set('theme', theme);
+            }
+            if(address){
+                newProm.set('address', address);
+                // Geocode address with google api
+                var geocoder = new google.maps.Geocoder();
+                console.log("Geocoding: "+address);
+                geocoder.geocode( { 'address': address}, function(results, status) {
+                  if (status == google.maps.GeocoderStatus.OK) {
+                    var loc_lat = results[0].geometry.location.lat();
+                    var loc_long = results[0].geometry.location.lng();
+                    var location = new Parse.GeoPoint({latitude: loc_lat, longitude: loc_long});
+                    newProm.set('preciseLocation', location);
+                    newProm.save(); // Save again, asynch, so may not be set for initial save
+                  } else {
+                      console.log("Error with geocoding string." + status);
+                  }
+                });
             }
             newProm.save().then(function(){
                 Parse.User.current().addUnique('proms', newProm);
@@ -375,6 +397,7 @@ $(function() {
                     newStore.set('address', address);
                     // Geocode address with google api
                     var geocoder = new google.maps.Geocoder();
+                    console.log("Geocoding: "+address);
                     geocoder.geocode( { 'address': address}, function(results, status) {
                       if (status == google.maps.GeocoderStatus.OK) {
                         var loc_lat = results[0].geometry.location.lat();
@@ -382,7 +405,9 @@ $(function() {
                         var location = new Parse.GeoPoint({latitude: loc_lat, longitude: loc_long});
                         newStore.set('location', location);
                         newStore.save(); // Save again, asynch, so may not be set for initial save
-                      } 
+                      } else {
+                          console.log("Error with geocoding string." + status);
+                      }
                     });
                 }
                 return newStore.save();
@@ -402,9 +427,6 @@ $(function() {
             // Set required fields
             newStore.set('name', name);
             // Set optional fields (We know at least one contact field should be filled)
-            if(address){
-                newStore.set('address', address);
-            }
             if(phone){
                 newStore.set('phone', phone);
             }
@@ -414,6 +436,24 @@ $(function() {
             if(hours){
                 newStore.set('hours', hours);
             }
+            if(address){
+                newStore.set('address', address);
+                // Geocode address with google api
+                var geocoder = new google.maps.Geocoder();
+                console.log("Geocoding: "+address);
+                geocoder.geocode( { 'address': address}, function(results, status) {
+                  if (status == google.maps.GeocoderStatus.OK) {
+                    var loc_lat = results[0].geometry.location.lat();
+                    var loc_long = results[0].geometry.location.lng();
+                    var location = new Parse.GeoPoint({latitude: loc_lat, longitude: loc_long});
+                    newStore.set('location', location);
+                    newStore.save(); // Save again, asynch, so may not be set for initial save
+                  } else {
+                      console.log("Error with geocoding string." + status);
+                  }
+                });
+            }
+
             newStore.save().then(function(){
                 Parse.User.current().addUnique('stores', newStore);
                 return Parse.User.current().save();
@@ -442,10 +482,9 @@ $(function() {
         store:saveNewStore
     };
 
-    /* The following are specific implementations of editors for each data type.
-     * A generic editor may be possible in the future. Currently this uses the 
-     * abstract editor template, but assigns the onclick functions in a hard-coded way
-     */
+    /* View Objects (Backbone View - Underscore template) ******************/
+
+    /* Editor object. I used to create dresses, proms, and stores */
     var Editor = Parse.View.extend({
         initialize: function(options){
             this.edittype = options.edittype // Key to represent desired editor style
@@ -464,6 +503,80 @@ $(function() {
             this.$('.btn-cancel').click(cancelHandlers[this.edittype]);
         }
     });
+
+    /* PromDetailRow:
+    *   Defines a row of a table that displays detailed information about a prom
+    *   in the search results area. */
+    var PromDetailRow = Parse.View.extend({
+        tagName: 'tr',
+        initialize: function(options){
+            this.template = PromDetailRowTemplate;
+            this.options = options; // Options include a label and a value
+            this.render();
+        },
+        render: function(){
+            this.$el.html(this.template(this.options));
+            return this;
+        }
+    })
+    /* PromSearchItem 
+    *   template to be filled in with search data to act as search results */
+    var PromSearchItem = Parse.View.extend({
+        initialize: function(options){
+            this.template = PromSearchItemTemplate;
+            this.prom = options['prom'];
+            this.render();
+            this.fillDetails(); // Details are in a collapsed view, allow them to be loaded later
+        },
+        render: function(){
+            var data = {};
+            // First fill in required keys
+            data["promid"] = this.prom.id;
+            data['schoolName'] = prom.get('schoolName'); // Required field, shouldn't be nil
+            // Use template data to create DOM object
+            this.$el.html(this.template(data));
+            return this;
+        },
+        events: {
+            "click #follow": "attemptFollow"
+        },
+        fillDetails: function(){
+            // Fill in detailed keys
+            var readable = {locationDescription: "Venue",
+                        theme:'Theme',
+                        time:'Prom Time',
+                        address:'Address'
+                    };
+            var detailKeys = ['address', 'locationDescription','theme','time'];
+            for (var key of detailKeys) {
+                var value = this.prom.get(key);
+                if(value){
+                    var label = readable[key];
+                    var detailItem = new PromDetailRow({'label':label, 'value':value});
+                    this.$('#detail-table').append(detailItem.el);
+                }
+            }
+        },
+        attemptFollow: function(){
+            var current = Parse.User.current();
+            if(current){
+                current.fetch().then(function(){
+                    current.addUnique("proms", this.prom);
+                    current.save();
+                }, function(error){
+                    //Error fetching user, try once more
+                    current.fetch().then(function(){
+                        displayProfile(current);
+                    }, function(error){
+                        alert("Error getting user data, please check your internet connection and try again.");
+                    });
+                });   
+            } else {
+                alert("You must be signed in to go to this page.");
+                window.location = "https://www.prom-app.com";
+            }
+        }
+    })
 
     /* Given a type of object ("dress", "prom", "store") newForm displays
     *   the corresponding form for object creation. */
@@ -663,11 +776,15 @@ $(function() {
             storeInfo_thumb_caption.setAttribute('class', 'caption');
             var caption_name = document.createElement('h4');
             var caption_address = document.createElement('h5');
+            var caption_website = document.createElement('h5');
+            var caption_website_link = document.createElement('a');
             //Retrieve data from model to fill in information
             store.fetch({
               success: function(myObject) {
                 caption_name.innerHTML = store.get("name");
-                caption_address.innerHTML = store.get("website");
+                caption_address.innerHTML = store.get("address");
+                caption_website_link.innerHTML = store.get("website");
+                caption_website_link.setAttribute("href", store.get("website"));
                 //Retrieve stored picture from model
                 var storePic = store.get("image");
                 if (storePic) {
@@ -686,6 +803,8 @@ $(function() {
             //Add children to parent elements
             storeInfo_thumb_caption.appendChild(caption_name);
             storeInfo_thumb_caption.appendChild(caption_address);
+            caption_website.appendChild(caption_website_link);
+            storeInfo_thumb_caption.appendChild(caption_website);
             storeInfo_thumb.appendChild(storeInfo_thumb_img);
             storeInfo_thumb.appendChild(storeInfo_thumb_caption);
             storeInfo.appendChild(storeInfo_thumb);
@@ -707,11 +826,74 @@ $(function() {
         }   
     }
 
+    /* Prom search functions for modal dialog*/
+    /* Given a string, the following function returns a list of proms
+     * with similar names.*/
+    function loadPromListFromSearch(search){
+        // Split search string into individual words
+        var search_tokens = search.toLowerCase().split(" ");
+        console.log("Search tokens: "+search_tokens);
+        // Define words to ignore (i.e. don't get every name with "high school")
+        var common_words = ["high", "school", "academy"];
+        // Create small queries for each token of search
+        var queries = [];
+        for (token of search_tokens){
+            if(common_words.indexOf(token) == -1){
+                var q = new Parse.Query(Prom);
+                q.contains("searchName", token);
+                queries.push(q);
+            }
+        }
+        if(queries.length == 0){
+            console.log("Not able to retrieve valid query from search.");
+            showError('#promsearchalert', 'Search did not return any results. Generic words such as "high school" or "academy" will be ignored.');
+        } else {
+            // Create query to find data from Parse Server
+            var query = queries.pop(); // Get an initial query to start "or"-ing with other queries
+            for (tokenq of queries){
+                query = Parse.Query.or(query, tokenq);
+            }
+            query.limit(QUERY_LIMIT);
+            query.find({
+              success: function(results) {
+                console.log("Found " + results.length + " Prom objects from search.");
+                if(results.length > 0){
+                    for (prom of results){
+                        var listView = new PromSearchItem({'prom': prom});
+                        $('#promsearchresults').append(listView.el);
+                    }
+                } else {
+                    showError('#promsearchalert', 'Search did not return any results.\nNote: words such as "high" "school" and "academy" are ignored.');
+                }
+              },
+              error: function(error) {
+                alert("Error: " + error.code + " " + error.message);
+              }
+            });
+        }
+    }
+    /* Event handler for click event of search button inside of prom search dialog */
+    function modalPromSearch(){
+        $('#promsearchresults').empty(); // Clear out old results
+        clearError('#promsearchalert'); // Clear any existing errors
+        var searchString = $('#search-prom-field').val();
+        if(searchString && searchString != ""){
+            loadPromListFromSearch(searchString);
+        } else {
+            console.log('Attempted to search for null or empty string.');
+            showError('#promsearchalert', 'Please enter a valid search.');
+        }
+    }
 
     loadProfile();
     $('#add_dress').click(function(){newForm("dress");});
     $('#add_prom').click(function(){newForm("prom");});
     $('#add_store').click(function(){newForm("store");});
+
+    // Search button in modal view
+    $('#search-prom-btn').click(modalPromSearch);
+    // Button that dismisses a modal view -> reload profile
+    $('.dismiss').click(loadProfile);
 });
 
 
